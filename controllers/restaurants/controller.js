9156035a938833e1b14b1fe4c571cf433daaf1b2
Alessa Messineo <apmessineo@gmail.com>
2017-03-17 15:33:55 -0400

    change API request to YELP

--- controllers/restaurants/controller.js
+++ controllers/restaurants/controller.js
@@ -1,5 +1,4 @@
-const Restaurant = require('../../models/restaurant.js');
-const GooglePlaces = require('../../services/restaurants.js');
+const Yelp = require('../../services/restaurants.js');
 
 const controller = {};
 
@@ -7,56 +6,14 @@ const controller = {};
 controller.index = (req, res) => {
   // console.log('req.params.zip: ', req.params.zip)
   console.log('req.params ', req.params)
-  GooglePlaces.search(req.params.zip, req.params.category)
+  Yelp.search(req.params.lat, req.params.long, req.params.term)
   .then(r => r.json()
   .then((data) => {
     console.log('******data.: ', data);
-    res.send(data.results);
+    res.send(data);
   }))
   .catch(err => console.log(err));
 };
-
-//controller.show will give us all the favorites
-//of a certain user
-controller.show = (req, res) => {
-  Restaurant.findAllById(req.params.id)
-  .then((data) => {
-    res.send(data);
-  })
-  .catch((err) => console.log(err))
-
-}
-
-//controller.create will add to the favorites
-//of a certain user
-controller.create = (req, res) => {
-  Restaurant.create(req.body.restaurant, req.params.user_id)
-  //assuming we receive an object with {restaurant: {restaurant details}}
-  .then((data) => {
-    console.log('controller.create is working');
-    res.sendStatus(201);
-  })
-  .catch((err) => console.log(err))
-
-}
-
-controller.update = (req, res) => {
-  Restaurant.update(req.body.restaurant, req.params.user_id, req.params.restaurant_id)
-  .then((data) => {
-    console.log('controller.update is working')
-    res.sendStatus(200);
-  })
-  .catch((err) => console.log(err))
-}
-
-//controller.destroy deletes a favorite from a user
-controller.destroy = (req, res) => {
-  Restaurant.destroy(req.params.restaurant_id)
-  .then(() => {
-    res.sendStatus(200);
-  })
-  .catch((err) => console.log(err))
-}
 
 
 module.exports = controller;
--- controllers/restaurants/index.js
+++ controllers/restaurants/index.js
@@ -3,12 +3,12 @@ const router = express.Router();
 
 const controller = require('./controller');
 
-router.get('/zip/:zip/:category', controller.index);
-router.get('/:user_id', controller.show);
-router.post('/:user_id', controller.create);
+router.get('/:lat/:long/:term', controller.index);
+// router.get('/:user_id', controller.show);
+// router.post('/:user_id', controller.create);
 
-router.put('/:user_id/:restaurant_id', controller.update);
-router.delete('/:user_id/:restaurant_id', controller.destroy);
+// router.put('/:user_id/:restaurant_id', controller.update);
+// router.delete('/:user_id/:restaurant_id', controller.destroy);
 
 
 module.exports = router;
