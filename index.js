bf77ba289a57c4d1594792bc1a9cd6812a796834
Alessa Messineo <apmessineo@gmail.com>
2017-03-17 15:44:32 -0400

    tweak to index.js

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const app = express();
 
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt');
-const methodOverride = require('method-override');
 
 require('dotenv').config();
 
@@ -14,7 +13,6 @@ app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-// app.use(methodOverride('_method'));
 
 app.use(require('./resources.js'));
 
