dcee64db0505c24f4cb932b7b53a6a88275a7765
Alessa Messineo <apmessineo@gmail.com>
2017-03-17 22:45:36 -0400

    remove unnecessary controller functions

--- controllers/restaurants/controller.js
+++ controllers/restaurants/controller.js
@@ -5,11 +5,11 @@ const controller = {};
 
 controller.index = (req, res) => {
   // console.log('req.params.zip: ', req.params.zip)
-  console.log('req.params ', req.params)
+  // console.log('req.params ', req.params)
   Yelp.search(req.params.lat, req.params.long, req.params.term)
   .then(r => r.json()
   .then((data) => {
-    console.log('******data.: ', data);
+    // console.log('******data.: ', data);
     res.send(data);
   }))
   .catch(err => console.log(err));
