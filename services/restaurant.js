6f339950c8a2d9f59d0c284553141f09a39a5dcd
Marcel Hamel <marcel.hamel@gmail.com>
2017-04-20 08:29:17 -0400

    ADD limit param to API call

--- services/restaurant.js
+++ services/restaurant.js
@@ -6,7 +6,7 @@ const Token = process.env.access_token;
 
 yelp.search = (lat, long, term) => {
   console.log('search string- term, lat, long', term, lat, long);
-  return fetch(`https://api.yelp.com/v3/businesses/search?term=${term}&radius=403&latitude=${lat}&longitude=${long}&radius_filter=403`, {
+  return fetch(`https://api.yelp.com/v3/businesses/search?term=${term}&radius=403&latitude=${lat}&longitude=${long}&radius_filter=403&limit=50`, {
     method: 'GET',
     headers: {
       'Content-Type': 'application/x-www-form-urlencoded',
