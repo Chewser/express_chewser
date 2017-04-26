926f5712367f97721c59cf7f2d9420c634f28315
Alessa Messineo <apmessineo@gmail.com>
2017-04-26 11:59:59 -0400

    change to package.json and convert request to Axios

--- services/restaurant.js
+++ services/restaurant.js
@@ -4,7 +4,7 @@ const yelp = {};
 const Token = process.env.access_token;
 
 yelp.search = (lat, long, term) => {
-  console.log('search string- term, lat, long', term, lat, long);
+
   return fetch(`https://api.yelp.com/v3/businesses/search?term=${term}&radius=403&latitude=${lat}&longitude=${long}&radius_filter=403`, {
     method: 'GET',
     headers: {
